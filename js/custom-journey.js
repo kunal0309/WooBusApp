2f5d7957c9388a01d834726de678e632746dae96
pariskshitdutt <pdutt111>
2015-10-30 02:20:44 +0530

    journey data resolved

---
+++ js/custom-journey.js
@@ -0,0 +1,41 @@
+var email = "", password = "", phonenumber = "";
+
+function getRouteDetails() {
+    var token = sessionStorage.getItem("access_token");
+    var headers = {};
+    if (token) {
+        headers.Authorization = 'Bearer ' + token;
+    }
+
+    $.ajax({
+        type: 'GET',
+        url: _apiBaseUrl + '/route/currentRoute',
+        dataType: "json",
+        headers: headers,
+        success: dataParserUser,
+        error: ServiceError
+    });
+
+    function dataParserUser(data) {
+        var journeyData= JSON.stringify(data);
+        var data=JSON.parse(journeyData);
+        console.log(data.start);
+        $("#from").html(data.start);
+        $("#to").html(data.end);
+        $("#distance").html(data.distance+" Kms");
+        var time_taken = moment.duration(data.time_taken, 'milliseconds').asHours;
+        $("#time").html(time_taken);
+        console.log(journeyData);
+        sessionStorage.setItem('journey', journeyData);
+        //window.location.href = "profile.html";
+    }
+}
+
+$(document).ready(function () {
+    console.log("running");
+    getRouteDetails();
+setInterval(function(){
+    getRouteDetails();
+},30000);
+});
+
